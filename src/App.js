bf73daec4b67ecf3a2486dfafd3d10677b7af750
Miriam Ertl <miriam_ertl@web.de>
2023-08-24 15:16:52 +0200

    add useEffect/useState function to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from "./logo.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [numberInSpace, setNumberInSpace] = useState(0);
+
+  useEffect(() => {
+    async function peopleInSpace() {
+      const response = await fetch();
+      const numberInSpace = await response.json(
+        "http://api.open-notify.org/astros.json"
+      );
+      setNumberInSpace(numberInSpace);
+    }
+    peopleInSpace();
+  }, []);
 
   return (
     <div className="App">
