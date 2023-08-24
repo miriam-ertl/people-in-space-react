36b2268e7ef2a10018758d0a4934346deecbab86
Miriam Ertl <miriam_ertl@web.de>
2023-08-24 16:04:01 +0200

    add namesInSpace as useState to async fetch function

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 
 function App() {
   const [numberInSpace, setNumberInSpace] = useState(0);
+  const [namesInSpace, setNamesInSpace] = useState([]);
 
   useEffect(() => {
     async function peopleInSpace() {
       const response = await fetch("http://api.open-notify.org/astros.json");
       const data = await response.json();
       setNumberInSpace(data.number);
+      setNamesInSpace(data.people);
     }
     peopleInSpace();
   }, []);
@@ -17,6 +19,13 @@ function App() {
   return (
     <main>
       <h1 className="h1">People in Space: {numberInSpace}</h1>
+      {namesInSpace.length > 0 && (
+        <ul>
+          {namesInSpace.map((name) => (
+            <li key={name.name}>{name.name}</li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
